77fed1cd564a185930f145efd6529afd75748f5b
Sundar Karthik S <sundar1007karthik@gmail.com>
2026-01-28 22:30:17 +0530

    first commit init

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,51 @@
+package com.pm.patientservice.service;
+
+
+import com.pm.patientservice.dto.PatientRequestDTO;
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.exception.EmailAlreadyExistsException;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+
+//        List<PatientResponseDTO> patientRepsonseDTo = patients.stream()
+//                .map(patient -> PatientMapper.toDTO(patient))
+//                .toList();
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream()
+                .map(PatientMapper::toDTO)
+                .toList();
+
+        return patientResponseDTOs;
+    }
+
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(
+                patientRequestDTO.getEmail()
+        )){
+            throw new EmailAlreadyExistsException("A patient with this email" +
+                    "already exists" + patientRequestDTO.getEmail());
+        }
+
+        Patient patient = PatientMapper.toModel(patientRequestDTO);
+        Patient savePatient = patientRepository.save(patient);
+        return PatientMapper.toDTO(savePatient);
+    }
+
+}
