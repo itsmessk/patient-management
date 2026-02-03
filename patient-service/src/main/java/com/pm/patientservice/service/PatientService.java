f17dc7a4b6a0308d306eca906b084cf9fe7019f2
Sundar Karthik S <sundar1007karthik@gmail.com>
2026-02-03 20:07:31 +0530

    kafka init and ongoing

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,6 +5,8 @@ import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
+import com.pm.patientservice.kafka.KafkaProducer;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
@@ -17,10 +19,19 @@ import java.util.UUID;
 @Service
 public class PatientService {
 
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+
+
     private final PatientRepository patientRepository;
 
-    public PatientService(PatientRepository patientRepository){
+    private final KafkaProducer kafkaProducer;
+
+    public PatientService(PatientRepository patientRepository,
+                          BillingServiceGrpcClient billingServiceGrpcClient,
+                          KafkaProducer kafkaProducer){
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.kafkaProducer = kafkaProducer;
     }
 
 
@@ -48,6 +59,9 @@ public class PatientService {
 
         Patient patient = PatientMapper.toModel(patientRequestDTO);
         Patient savePatient = patientRepository.save(patient);
+
+        billingServiceGrpcClient.createBillingAccount(patient.getId().toString(), patient.getName(), patient.getEmail());
+        kafkaProducer.sendEvent(patient);
         return PatientMapper.toDTO(savePatient);
     }
 
@@ -74,4 +88,7 @@ public class PatientService {
     public void deletePatient(UUID id){
         patientRepository.deleteById(id);
     }
+
+
+
 }
